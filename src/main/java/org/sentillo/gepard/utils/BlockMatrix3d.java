3d833e1509e58f8e51039f7e69e4e85c8abeddea
bfalecki <01158532@pw.edu.pl>
2022-11-26 22:08:42 +0100

    Rozszerzenie klasy BlockMatrix3d

--- src/main/java/org/sentillo/gepard/utils/BlockMatrix3d.java
+++ src/main/java/org/sentillo/gepard/utils/BlockMatrix3d.java
@@ -10,7 +10,37 @@ public class BlockMatrix3d extends Matrix3d<BlockType>{
             objects.put(vector3d, turnBlock90Left(objects.get(vector3d)));
         }
     }
+    @Override
+    public void turn90Right(){
+        super.turn90Right();
+        for(Vector3d vector3d: objects.keySet()){
+            objects.put(vector3d, turnBlock90Right(objects.get(vector3d)));
+        }
+    }
+    @Override
+    public void mirrorXAxis(){
+        super.mirrorXAxis();
+        for(Vector3d vector3d: objects.keySet()){
+            objects.put(vector3d, mirrorBlockX(objects.get(vector3d)));
+        }
+    }
+    @Override
+    public void mirrorYAxis(){
+        super.mirrorYAxis();
+        for(Vector3d vector3d: objects.keySet()){
+            objects.put(vector3d, mirrorBlockY(objects.get(vector3d)));
+        }
+    }
     private BlockType turnBlock90Left(BlockType blockType){
-
+        return blockType.rotateLeft(blockType);
+    }
+    private BlockType turnBlock90Right(BlockType blockType){
+        return blockType.rotateRight(blockType);
+    }
+    private BlockType mirrorBlockX(BlockType blockType){
+        return blockType.mirrorX(blockType);
+    }
+    private BlockType mirrorBlockY(BlockType blockType){
+        return blockType.mirrorY(blockType);
     }
 }
